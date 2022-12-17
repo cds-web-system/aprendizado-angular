9ed995b3bb5f76a2f3a90871bc088d00f42a2c1c
CDS WebSystem <cdswebsystem@gmail.com>
2022-12-17 10:55:53 -0300

    Adicionando templateUrl no app.module.js

--- primeiro-projeto/src/app/primeiro-component/primeiro.component.ts
+++ primeiro-projeto/src/app/primeiro-component/primeiro.component.ts
@@ -9,11 +9,13 @@ import {Component} from '@angular/core'
 @Component({
   // tag para o html ex: no app.component.html basta add <primeiro-component></primeiro-component>
   selector: 'primeiro-component',
-  //template do html
-  template: `
-            <p>Meu Primeiro Component Angular</p>
-            `
+  //template do html via arquivo
+  templateUrl: './primeiro.component.html',
+  //template do html via tag, mas quando é add o templateUrl ele desconsidera o template
+  template: `<p>Teste template via tag</p>`
 })
 
 // devemos importar no app.module.ts o nome da classa abaixo
-export class PrimeiroComponent{}
+export class PrimeiroComponent{
+  teste = "Teste template via arquivo html";
+}
